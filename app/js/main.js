8688ad9328fa7a7063da6821b81872d868a51689
Lvov17151991 <lvov1715kharkiv@gmail.com>
2023-01-30 23:21:08 +0200

    divided into sections and fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function () {
    $('.comments__slider').slick({
-    dots: true
+      dots: true,
+      appendArrows: $('.slic-nav'),
+      appendDots: $('.slic-nav'),
+      autoplay: true,
+      autoplaySpeed: 5000,
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      pauseOnDotsHover: true,
    });
+
+   var mixer = mixitup('.gallery');
 })
